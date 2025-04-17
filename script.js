d5716733a69ee7509039b8f66364b42d16f4a3ff
Vingroupfirma <vingroupfirma@gmail.com>
2025-04-18 00:32:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Scroll reveal
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.scroll-fade');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  elements.forEach(el => observer.observe(el));
+});

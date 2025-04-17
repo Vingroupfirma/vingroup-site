4fa1aca3fe6c0a42ce8d108544dd076a4fb65e9e
Vingroupfirma <vingroupfirma@gmail.com>
2025-04-17 22:02:04 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const fadeInElements = document.querySelectorAll(".fade-in");
-  fadeInElements.forEach((el, index) => {
-    el.style.animationDelay = `${index * 0.3}s`;
-    el.classList.add("visible");
-  });
-});

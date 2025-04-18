b9bb9db191751aff072335d2209c2b348a002c4b
Vingroupfirma <vingroupfirma@gmail.com>
2025-04-18 12:14:22 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Scroll animation (optional basic setup)
-window.addEventListener("scroll", () => {
-  document.querySelectorAll('.card, .fade-in').forEach(el => {
-    const rect = el.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 50) {
-      el.classList.add("fade-in");
-    }
-  });
-});
